d77aae0fb784b0c5b0826be0bf089dcdb273e2e5
dickypp27 <dickypermataputra@gmail.com>
2022-10-27 21:34:51 +0700

    Tugas Javascript

---
+++ app.js
@@ -0,0 +1,25 @@
+// TUGAS 1
+
+// Menampilkan teks ke console javascript
+console.log('Hello, World');
+// 
+console.debug('Hello, World');
+// Menampilkan pesan ke console javascript
+console.info('Hello, World');
+// Menampilkan info teks ke console javascript
+console.error('Hello, World');
+// Menampilkan simbol error ke console javascript
+console.dir('Hello, World');
+// Menampilkan daftar interaktif ke console javascript
+alert('Hello, World');
+
+// TUGAS 2 MIN-MAX
+let arr = [0, 1, 2, 3, 4, 5];
+
+// TUGAS 3
+var arr = [];
+arr[1] = "Senter";
+arr[2] = "Radio";
+arr[3] = "Antenna";
+arr[4] = "Obeng";
+arr[5] = "Kunci"
